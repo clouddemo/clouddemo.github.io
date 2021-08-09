808aa45d3b9fb8c234b323a7907eecad88c55bb0
clouddemo <ritakai@aliyun.com>
2021-08-09 23:39:46 +0800

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# clouddemo.github.io
---
+++ wmb/fb/Interface.ProfiNET.Manager.py
@@ -0,0 +1,9 @@
+import json
+import socket
+import sys
+import time
+
+def SPIDR_FB_Main(input):
+    output = {"data1":""}
+    status = 0x00000000
+    return (output, status)
